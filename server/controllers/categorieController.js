582dfe9961aadea6db4869720437700dc653c5bc
Lautaro Nievas <lautynievas09@gmail.com>
2022-04-22 17:29:11 -0300

    Client

--- server/controllers/categorieController.js
+++ server/controllers/categorieController.js
@@ -1,16 +1,16 @@
 const model = require('../models/categorieModel')
-
-function getAll (req,res){
-  const [categories] = model.getAll(req,res);
-  if(!categories){
-    res.send(categories)
-  }
-}
+const db = require('../kc')
 
 module.exports = {
-  getAll,
+  getAll:(req,res) =>{
+    db.query(
+      "SELECT * FROM categories",
+      (err, result) => {
+        res.send(result);
+      }
+    );
+  }
 }
-
 
 
 
